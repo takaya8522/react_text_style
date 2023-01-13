cd0a135468e3e66f555b418bb5a744a7b78ddcd8
takaya suzuki <suzuki8522@yahoo.co.jp>
2023-01-13 12:06:11 +0900

    🔧cssファイルをimportする

--- src/020_import_css/start/Example.js
+++ src/020_import_css/start/Example.js
@@ -1,25 +1,14 @@
 import { useState } from "react";
+import "./Example.css"
 
 const Example = () => {
   const [isSelected, setIsSelected] = useState(false);
 
   const clickHandler = () => setIsSelected((prev) => !prev);
 
-  const style = {
-    margin: "auto",
-    "border-radius": "9999px",
-    border: "none",
-    display: "block",
-    width: 120,
-    height: 60,
-    fontWeight: "bold",
-    cursor: "pointer",
-    backgroundColor: isSelected ? "pink" : "",
-  };
-
   return (
     <>
-      <button style={style} onClick={clickHandler}>
+      <button className={`btn ${isSelected ? "selected" : ""}`} onClick={clickHandler}>
         ボタン
       </button>
       <div style={{ textAlign: "center" }}>
