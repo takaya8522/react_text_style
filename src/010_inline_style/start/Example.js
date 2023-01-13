7a423f33ddc409ff377b7e7f667cfabb31e57641
takaya suzuki <suzuki8522@yahoo.co.jp>
2023-01-13 11:29:33 +0900

    🔧インラインスタイルで記述

--- src/010_inline_style/start/Example.js
+++ src/010_inline_style/start/Example.js
@@ -5,10 +5,22 @@ const Example = () => {
 
     const clickHandler = () => setIsSelected(prev => !prev);
 
+    const style = {
+        width: 120,
+        height: 60,
+        fontWeight: "bold",
+        cursor: "pointer",
+        backgroundColor: isSelected ? "pink" : "",
+        margin: "auto",
+        "border-radius": "9999px",
+        border: "none",
+        display: "block",
+    }
+
     return (
         <>
-            <button onClick={clickHandler}>ボタン</button>
-            <div>{isSelected && "クリックされました。"}</div>
+            <button style={style} onClick={clickHandler}>ボタン</button>
+            <div style={{ textAlign: "center" }} >{isSelected && "クリックされました。"}</div>
         </>
     )
 };
